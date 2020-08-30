e2efaf7f4a8f48017a1e78dcd773a08b97dcf527
Fabio Biasi Mello Rubim <fabio.rubim-terceiros@contmatic.com.br>
2020-08-30 14:32:32 -0300

    Aula 29 - Adicionados botões

--- lib/main.dart
+++ lib/main.dart
@@ -6,12 +6,34 @@ main() => runApp(PerguntaApp());
 class PerguntaApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
+    //final List<String> perguntas = []; //Sem inferência
+    final perguntas = [
+      'Qual é sua cor favorita?',
+      'Qual é seu animal favorito?'
+    ];
+
     return MaterialApp(
       home: Scaffold(
         appBar: AppBar(
           title: Text('Perguntas'),
         ),
-        body: Text('Olá Flutter!'),
+        body: Column(
+          children: [
+            Text(perguntas[0]),
+            RaisedButton(
+              child: Text('Resposta 1'),
+              onPressed: null,
+            ),
+            RaisedButton(
+              child: Text('Resposta 2'),
+              onPressed: null,
+            ),
+            RaisedButton(
+              child: Text('Resposta 3'),
+              onPressed: null,
+            ),
+          ],
+        ),
       ),
     );
   }
