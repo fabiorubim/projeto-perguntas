3ff2c26c727599e511cdca20f6e171c0590f8464
Fabio Biasi Mello Rubim <fabio.rubim-terceiros@contmatic.com.br>
2020-08-30 13:00:43 -0300

    Aula 24

---
+++ lib/main.dart
@@ -0,0 +1,14 @@
+//import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+
+main() {
+  runApp(new PerguntaApp());
+}
+
+class PerguntaApp extends StatelessWidget {
+  Widget build(BuildContext context) {
+    return new MaterialApp(
+      home: Text('Olá Flutter!'),
+    );
+  }
+}
