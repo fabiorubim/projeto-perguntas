e2234b6126e74a890c7107e59d39f1866a3cb5e9
Fabio Biasi Mello Rubim <fabio.rubim-terceiros@contmatic.com.br>
2020-10-13 21:59:46 -0300

    Antes de tornar declarativo

--- lib/main.dart
+++ lib/main.dart
@@ -33,11 +33,13 @@ class _PerguntaAppState extends State<PerguntaApp> {
       }
     ];
 
-    List<Widget> respostas = [];
+    List<String> respotas = perguntas[_perguntaSelecionada]['respostas'];
+    List<Widget> widgets =
+        respotas.map((t) => Resposta(t, _responder)).toList();
 
-    for (String textoResp in perguntas[_perguntaSelecionada]['respostas']) {
-      respostas.add(Resposta(textoResp, _responder));
-    }
+    // for (String textoResp in respotas) {
+    //   widgets.add(Resposta(textoResp, _responder));
+    // }
 
     return MaterialApp(
       home: Scaffold(
@@ -47,7 +49,7 @@ class _PerguntaAppState extends State<PerguntaApp> {
         body: Column(
           children: [
             Questao(perguntas[_perguntaSelecionada]['texto']),
-            ...respostas,
+            ...widgets,
           ],
         ),
       ),
